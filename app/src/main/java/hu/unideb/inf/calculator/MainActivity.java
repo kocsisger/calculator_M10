792c00ae8ccfb55aede284c681618c4815ff6e88
Dr. John H. Watson <john.watson@inf.unideb.hu>
2026-02-16 11:24:08 +0100

    expression is split

--- app/src/main/java/hu/unideb/inf/calculator/MainActivity.java
+++ app/src/main/java/hu/unideb/inf/calculator/MainActivity.java
@@ -1,6 +1,7 @@
 package hu.unideb.inf.calculator;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
@@ -33,8 +34,22 @@ public class MainActivity extends AppCompatActivity {
         Button button = (Button)view;
         switch (button.getText().toString()){
             case "CE": resultTextView.setText("0"); break;
-            case "0" : break;
+            case "=" : resultTextView.setText(
+                           calculate(resultTextView.getText().toString())
+                       ); break;
             default : resultTextView.append(button.getText());
         }
+    }
+
+    private String calculate(String expression) {
+        String op1_str = expression.split("[+-/*]")[0];
+        int op1 = Integer.parseInt(op1_str);
+        int op2 = Integer.parseInt(expression.split("[+-/*]")[1]);
+
+        char operator = expression.charAt(op1_str.length());
+
+         Log.d("TEST_OP", "Operands: " + op1 + " "  + operator + " " + op2);
+
+         return "-";
     }
 }
